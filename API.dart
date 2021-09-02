3586fc0898e0c96e11999c4ee540d1307a76f7d0
Manzur Ahmad Mitha Khan <31978008+Mithakhan@users.noreply.github.com>
2021-09-02 05:50:47 +0500

    Update API.dart

--- API.dart
+++ API.dart
@@ -1,20 +1,37 @@
-import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-Future getdata(url) async {
- 
-   try {
-      http.Response response = await http.get(url);
-      if (response.statusCode == 200) {
-        print("DATA FETCHED SUCCESSFULLY");
-        var result = jsonDecode(response.body);
-        print(result["prediction"]);
-        return result["prediction"];
-      }
-    } catch (e) {
-      print("EXCEPTION OCCURRED: $e");
-      return null;
-  
+class Poetry {
+  final String data;
+  final String label;
+
+  Poetry({required this.data, required this.label});
+
+  factory Poetry.fromJson(Map<String, dynamic> json) {
+    return Poetry(
+      data: json['Data'],
+      label: json['Label'],
+    );
+  }
 }
+
+Future<Poetry> makeprediction(String data) async {
+  final response = await http.post(
+    Uri.parse('http://10.0.2.2:12345/predict/'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'Data': data,
+    }),
+  );
+  if (response.statusCode == 201) {
+    // If the server did return a 201 CREATED response,
+    // then parse the JSON.
+    return Poetry.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 201 CREATED response,
+    // then throw an exception.
+    throw Exception('Failed to make prediction.');
+  }
 }
